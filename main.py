1f69fe3eac878ff6a64b0a3dc4ce659231155b53
ka3vas <tyrnis@gmail.com>
2024-05-07 17:49:06 +0200

    count_letters function added

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def main():
-    letters_count = {"a": 0}
-
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path) 
-    # num_words = count_words(text)
-    # print(f"{num_words} words found in the document")
+    num_words = count_words(text)
+    letters_count = count_letters(text)
 
     print(letters_count)
 
@@ -15,5 +13,19 @@ def get_book_text(path):
 # this function could be called get_num_words
 def count_words(string):
     return len(string.split())
+
+# get_chars_dict
+def count_letters(string):
+    dictionary  = {}
+
+    for sign in string:
+        sign_lower = sign.lower()
+        
+        if sign_lower in dictionary:
+            dictionary [sign_lower] += 1
+        else:
+            dictionary [sign_lower] = 1
+
+    return dictionary        
 
 main()
